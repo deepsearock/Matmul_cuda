fcd76c92c371beb8e454ded156975f8978d2675c
deepsearock <zhaorui.p.wang@gmail.com>
2025-03-01 17:31:40 -0500

    change

--- src/comparison.cu
+++ src/comparison.cu
@@ -47,13 +47,13 @@ int main(int argc, char *argv[]) {
         float sharedTime = 0.0f;
         dim3 gridDim_shared((N + blockSize - 1) / blockSize, (M + blockSize - 1) / blockSize);
         dim3 blockDim_shared(blockSize, blockSize);
-        sharedTflops = matrixMultiplyShared<<<gridDim_shared, blockDim_shared>>>(A, B, C, M, K, N, blockSize, &sharedTime);
+        double sharedTflops = matrixMultiplyShared<<<gridDim_shared, blockDim_shared>>>(A, B, C, M, K, N, blockSize, &sharedTime);
         
         // Naive Kernel Execution
         float naiveTime = 0.0f;
         dim3 gridDim_naive((N + blockSize - 1) / blockSize, (M + blockSize - 1) / blockSize);
         dim3 blockDim_naive(blockSize, blockSize);
-        naiveTflops = matrixMultiplyNaive<<<gridDim_naive, blockDim_naive>>>(A, B, C, M, K, N, blockSize, &naiveTime);
+        double naiveTflops = matrixMultiplyNaive<<<gridDim_naive, blockDim_naive>>>(A, B, C, M, K, N, blockSize, &naiveTime);
         
         printf("%-12d %-20.3f %-20.3f %-20.3f %-20.3f %-20d %-20d %-20.2f %-20.2f\n", blockSize, sharedTflops, sharedTime, naiveTflops, naiveTime, theoreticalWarps, achievedWarps, theoreticalOccupancy, achievedOccupancy);
     }
