63e6c2f9fc6e950d294c882fbcd0cc4d37e16053
deepsearock <zhaorui.p.wang@gmail.com>
2025-03-02 01:52:17 -0500

    change

--- src/matrix_multiply_tiled.cuh
+++ src/matrix_multiply_tiled.cuh
@@ -69,13 +69,13 @@ inline std::pair<double, double> runMatrixMulTiled(int M, int N, int K, int tile
     auto result = measurePerformance([&]() {
         switch (tileSize) {
             case 8:
-                matrixMulTiled<8><<<dim3((N + 31) / 32, (M + 31) / 32), dim3(32, 32)>>>(d_A, d_B, d_C, M, N, K);
+                matrixMulTiled<8><<<dim3((N + 31) / 32, (M + 31) / 32), dim3(8, 32)>>>(d_A, d_B, d_C, M, N, K);
                 break;
             case 16:
-                matrixMulTiled<16><<<dim3((N + 31) / 32, (M + 31) / 32), dim3(32, 16)>>>(d_A, d_B, d_C, M, N, K);
+                matrixMulTiled<16><<<dim3((N + 31) / 32, (M + 31) / 32), dim3(16, 32)>>>(d_A, d_B, d_C, M, N, K);
                 break;
             case 32:
-                matrixMulTiled<32><<<dim3((N + 31) / 32, (M + 31) / 32), dim3(32, 8)>>>(d_A, d_B, d_C, M, N, K);
+                matrixMulTiled<32><<<dim3((N + 31) / 32, (M + 31) / 32), dim3(32, 32)>>>(d_A, d_B, d_C, M, N, K);
                 break;
             default:
                 std::cerr << "Unsupported tile size!" << std::endl;
