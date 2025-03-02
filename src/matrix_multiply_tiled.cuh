290d06fff6f81375db1d83f450bb2ecc6eafdfa2
deepsearock <zhaorui.p.wang@gmail.com>
2025-03-02 01:13:46 -0500

    change

--- src/matrix_multiply_tiled.cuh
+++ src/matrix_multiply_tiled.cuh
@@ -75,7 +75,7 @@ inline std::pair<double, double> runMatrixMulTiled(int M, int N, int K, int tile
                 matrixMulTiled<16><<<dim3((N + 31) / 32, (M + 31) / 32), dim3(32, 16)>>>(d_A, d_B, d_C, M, N, K);
                 break;
             case 32:
-                matrixMulTiled<32><<<dim3((N + 63) / 64, (M + 63) / 64), dim3(64, 8)>>>(d_A, d_B, d_C, M, N, K);
+                matrixMulTiled<32><<<dim3((N + 31) / 32, (M + 31) / 32), dim3(32, 8)>>>(d_A, d_B, d_C, M, N, K);
                 break;
             default:
                 std::cerr << "Unsupported tile size!" << std::endl;
