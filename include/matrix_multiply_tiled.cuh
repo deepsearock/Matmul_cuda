a1f5df5549ed0f3a0b646779460e3ba391b78097
deepsearock <zhaorui.p.wang@gmail.com>
2025-03-06 00:13:12 -0500

    asdasd

--- include/matrix_multiply_tiled.cuh
+++ include/matrix_multiply_tiled.cuh
@@ -129,14 +129,14 @@ inline std::pair<double, double> runMatrixMulTiled(int M, int N, int K, int tile
     }
 
     int threadsPerBlock = std::min(blockSize, 1024);  // Ensure we don't exceed max threads per block
-    dim3 blockDim(16, 16);
+    dim3 blockDim(32, 8);
     dim3 gridDim((N + blockDim.x - 1) / blockDim.x, (M + blockDim.y - 1) / blockDim.y);
 
     // Launch kernel using runtime-determined grid and block sizes
     auto result = measurePerformance([&]() {
         switch (tileSize) {
             case 8:
-                matrixMulTiled<32, 8, 16><<<gridDim, blockDim>>>(d_A, d_B, d_C, M, N, K);
+                matrixMulTiled<32, 8, 8><<<gridDim, blockDim>>>(d_A, d_B, d_C, M, N, K);
                 break;
             case 16:
                 matrixMulTiled<32, 16, 16><<<gridDim, blockDim>>>(d_A, d_B, d_C, M, N, K);
@@ -189,7 +189,7 @@ inline std::pair<double, double> runMatrixMulTiledWithErrorCheck(int M, int N, i
     }
 
     int threadsPerBlock = std::min(blockSize, 1024);  // Ensure we don't exceed max threads per block
-    dim3 blockDim(32, 16);
+    dim3 blockDim(32, 8);
     dim3 gridDim((N + blockDim.x - 1) / blockDim.x, (M + blockDim.y - 1) / blockDim.y);
 
     // Launch kernel using runtime-determined grid and block sizes
