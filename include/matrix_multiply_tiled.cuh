2874a736924fdaaa0c7423a77dbeb7e6740feef3
deepsearock <zhaorui.p.wang@gmail.com>
2025-03-05 23:51:48 -0500

    asdasd

--- include/matrix_multiply_tiled.cuh
+++ include/matrix_multiply_tiled.cuh
@@ -187,7 +187,7 @@ inline std::pair<double, double> runMatrixMulTiledWithErrorCheck(int M, int N, i
                 matrixMulTiled<32, 8, 16><<<gridDim, blockDim>>>(d_A, d_B, d_C, M, N, K);
                 break;
             case 32:
-                matrixMulTiled<32, 8 , 32><<<gridDim, blockDim>>>(d_A, d_B, d_C, M, N, K);
+                matrixMulTiled<32, 32, 32><<<gridDim, blockDim>>>(d_A, d_B, d_C, M, N, K);
                 break;
             default:
                 std::cerr << "Unsupported tile size" << std::endl;
