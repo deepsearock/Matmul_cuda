75ed12971f0e23400e4420dbf4b3ac5b12e7a3da
deepsearock <zhaorui.p.wang@gmail.com>
2025-03-06 23:14:30 -0500

    sdasdasd

--- src/comparison.cu
+++ src/comparison.cu
@@ -33,7 +33,7 @@ int main(int argc, char *argv[]) {
     std::cout << "  B (" << colDimA << "x" << colDimB << ")" << std::endl;
     std::cout << "  C (" << rowDimA << "x" << colDimB << ")" << std::endl;
 
-    int tileSizes[] = {16, 32, 64};
+    int tileSizes[] = {64};
     std::pair<int, int> blockConfigs[] = {{32, 8}, {16, 16}};
     int block_height_tile = 0;
     for (int tileSize : tileSizes) {
